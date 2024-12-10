7c1e7e106f223c2bdd87617c59e6e9db1c332ff0
fariaa01 <gustavofaria985@gmail.com>
2024-12-10 19:15:18 -0300

    aula25.cs

--- aula25.cs
+++ aula25.cs
@@ -1,30 +1,31 @@
-// using System.Diagnostics.CodeAnalysis;
-// using System.Globalization;
-// using System.Runtime.Serialization.Formatters;
-//
-// namespace programa25
-// {
-//     class programa25
-//     {
-//
-//         static void Main(string[] args){
-//
-//             // int n = int.Parse(Console.ReadLine());
-//
-//             // double []vect = new double[n];
-//
-//             // for(int i = 0; i < n; i++){
-//             //     vect[i] = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
-//             // }
-//
-//             // double sum = 0.0;
-//
-//             // for(int i = 0; i < n; i++){
-//             //     sum += vect[i];
-//             // }
-//
-//             // double avg = sum / n;
-//             // Console.WriteLine("A altura media é de: " + avg);
-//         } 
-//     }
-// }
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.Serialization.Formatters;
+
+namespace programa25
+{
+    class programa25
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vect = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            double sum = 0.0;
+
+            for (int i = 0; i < n; i++)
+            {
+                sum += vect[i];
+            }
+
+            double avg = sum / n;
+            Console.WriteLine("A altura media é de: " + avg);
+        }
+    }
+}
