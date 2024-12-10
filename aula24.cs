b729ec0bc8eadd4631a1700cf04a50ab976fd7af
fariaa01 <gustavofaria985@gmail.com>
2024-12-10 02:06:46 -0300

    Add files via upload

---
+++ aula24.cs
@@ -0,0 +1,20 @@
+
+using System.Drawing;
+
+namespace programa24s
+{
+    class programa24
+    {
+
+        static void Main(string[] args){
+            
+            Point p;
+
+            p.X = 10;
+            p.Y = 20;
+
+            Console.WriteLine(p);
+
+        } 
+    }
+}
---
+++ program24.cs
@@ -0,0 +1,13 @@
+namespace programa24
+{
+    struct Point
+    {
+      public double X;
+      public double Y;
+
+        public override string ToString()
+        {
+            return "(" + X + Y + ")";
+            }
+    }
+}
